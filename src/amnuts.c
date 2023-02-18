a7ce2cf4d9560297cff7e2c297f1e50582793746
Andrew Collington <andy@amnuts.com>
2023-02-18 15:04:42 +0000

    Updated some texts

--- src/amnuts.c
+++ src/amnuts.c
@@ -1,11 +1,10 @@
 /****************************************************************************
-         Amnuts version 2.3.0 - Copyright (C) Andrew Collington, 2003
-                      Last update: 2003-08-04
+             Amnuts - Copyright (C) Andrew Collington, 1996-2023
+                        Last update: Sometime in 2023
 
-                              talker@amnuts.net
-                          https://www.amnuts.net/
+                   talker@amnuts.net - https://amnuts.net/
 
-                                   based on
+                                 based on
 
    NUTS version 3.3.3 (Triple Three :) - Copyright (C) Neil Robertson 1996
  ***************************************************************************/
