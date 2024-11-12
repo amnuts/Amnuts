450071e97b83269afe6ccd938b0802b6f388a2b2
Andrew Collington <andy@amnuts.com>
2024-11-12 00:40:06 +0000

    Updated headers

--- src/includes/telnet.h
+++ src/includes/telnet.h
@@ -1,3 +1,14 @@
+/****************************************************************************
+             Amnuts - Copyright (C) Andrew Collington, 1996-2024
+                        Last update: Sometime in 2024
+
+                   talker@amnuts.net - https://amnuts.net/
+
+                                 based on
+
+   NUTS version 3.3.3 (Triple Three :) - Copyright (C) Neil Robertson 1996
+ ***************************************************************************/
+
 #ifndef AMNUTS_TELNET_H
 #define AMNUTS_TELNET_H
 
