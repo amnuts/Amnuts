cf666b3b918fd045cac9dfd469eff114f6eb4ad4
Andrew Collington <andy@amnuts.com>
2024-11-23 16:19:47 +0000

    Fixed typos

--- src/amnuts.c
+++ src/amnuts.c
@@ -2138,8 +2138,8 @@ sig_handler(int sig)
         switch (amsys->crash_action) {
         case 0:
             write_room(NULL,
-                    "\n\n\07~OLSYSTEM:~FR~LI WARNING - An illegal instruction has just occured!\n\n");
-            write_syslog(SYSLOG, 1, "WARNING: An illegal instruction occured!\n");
+                    "\n\n\07~OLSYSTEM:~FR~LI WARNING - An illegal instruction has just occurred!\n\n");
+            write_syslog(SYSLOG, 1, "WARNING: An illegal instruction occurred!\n");
             abort();
             break;
         case 1:
@@ -2177,8 +2177,8 @@ sig_handler(int sig)
         switch (amsys->crash_action) {
         case 0:
             write_room(NULL,
-                    "\n\n\07~OLSYSTEM:~FR~LI WARNING - An arithmetic exception has just occured!\n\n");
-            write_syslog(SYSLOG, 1, "WARNING: An arithmetic exception occured!\n");
+                    "\n\n\07~OLSYSTEM:~FR~LI WARNING - An arithmetic exception has just occurred!\n\n");
+            write_syslog(SYSLOG, 1, "WARNING: An arithmetic exception occurred!\n");
             abort();
             break;
         case 1:
@@ -2216,8 +2216,8 @@ sig_handler(int sig)
         switch (amsys->crash_action) {
         case 0:
             write_room(NULL,
-                    "\n\n\07~OLSYSTEM:~FR~LI WARNING - A segmentation fault has just occured!\n\n");
-            write_syslog(SYSLOG, 1, "WARNING: A segmentation fault occured!\n");
+                    "\n\n\07~OLSYSTEM:~FR~LI WARNING - A segmentation fault has just occurred!\n\n");
+            write_syslog(SYSLOG, 1, "WARNING: A segmentation fault occurred!\n");
             abort();
             break;
         case 1:
@@ -2255,8 +2255,8 @@ sig_handler(int sig)
         switch (amsys->crash_action) {
         case 0:
             write_room(NULL,
-                    "\n\n\07~OLSYSTEM:~FR~LI WARNING - A bus error has just occured!\n\n");
-            write_syslog(SYSLOG, 1, "WARNING: A bus error occured!\n");
+                    "\n\n\07~OLSYSTEM:~FR~LI WARNING - A bus error has just occurred!\n\n");
+            write_syslog(SYSLOG, 1, "WARNING: A bus error occurred!\n");
             abort();
             break;
         case 1:
--- src/netlinks.c
+++ src/netlinks.c
@@ -1095,11 +1095,11 @@ nl_user_exist(NL_OBJECT nl, char *to, char *from)
         user = get_user(from);
         if (user) {
             sprintf(text,
-                    "~OLSYSTEM:~RS An error occured during mail delivery to %s@%s.\n",
+                    "~OLSYSTEM:~RS An error occurred during mail delivery to %s@%s.\n",
                     to, nl->service);
             write_user(user, text);
         } else {
-            sprintf(text2, "An error occured during mail delivery to %s@%s.\n", to,
+            sprintf(text2, "An error occurred during mail delivery to %s@%s.\n", to,
                     nl->service);
             send_mail(NULL, from, text2, 0);
         }
@@ -1224,7 +1224,7 @@ nl_endmail(NL_OBJECT nl)
 }
 
 /*
- * An error occured at remote site
+ * An error occurred at remote site
  */
 void
 nl_mailerror(NL_OBJECT nl, char *to, char *from)
@@ -1234,11 +1234,11 @@ nl_mailerror(NL_OBJECT nl, char *to, char *from)
     user = get_user(from);
     if (user) {
         sprintf(text,
-                "~OLSYSTEM:~RS An error occured during mail delivery to %s@%s.\n",
+                "~OLSYSTEM:~RS An error occurred during mail delivery to %s@%s.\n",
                 to, nl->service);
         write_user(user, text);
     } else {
-        sprintf(text, "An error occured during mail delivery to %s@%s.\n", to,
+        sprintf(text, "An error occurred during mail delivery to %s@%s.\n", to,
                 nl->service);
         send_mail(NULL, from, text, 0);
     }
